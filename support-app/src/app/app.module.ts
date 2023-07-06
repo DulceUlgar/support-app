4acfa1581a0e0c72c46acad2cd5c48c1ac2d99c6
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-07-06 09:34:15 +0200

    changes

---
+++ support-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IndexComponent } from './index/index.component';
+import { EditrequestComponent } from './editrequest/editrequest.component';
+import { NewrequestComponent } from './newrequest/newrequest.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    IndexComponent,
+    EditrequestComponent,
+    NewrequestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
