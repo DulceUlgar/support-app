e0ce5bed992a1531a9771de013577d168efaa5c1
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-07-07 09:11:04 +0200

    creating funcionalitys

--- support-app/src/app/app.module.ts
+++ support-app/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { IndexComponent } from './index/index.component';
-import { EditrequestComponent } from './editrequest/editrequest.component';
-import { NewrequestComponent } from './newrequest/newrequest.component';
+import { IndexComponent } from './shared/index/index.component';
+import { EditrequestComponent } from './shared/editrequest/editrequest.component';
+import { NewrequestComponent } from './shared/newrequest/newrequest.component';
 
 @NgModule({
   declarations: [
