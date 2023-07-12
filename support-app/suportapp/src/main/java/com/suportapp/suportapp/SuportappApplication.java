39eaf2b33a4117da2de35a095a7d2f899e33df83
DulceUlgar <133334486+DulceUlgar@users.noreply.github.com>
2023-07-12 10:14:12 +0200

    creacion de springboot

---
+++ support-app/suportapp/src/main/java/com/suportapp/suportapp/SuportappApplication.java
@@ -0,0 +1,13 @@
+package com.suportapp.suportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuportappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuportappApplication.class, args);
+	}
+
+}
---
+++ support-app/suportapp/src/test/java/com/suportapp/suportapp/SuportappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suportapp.suportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuportappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
